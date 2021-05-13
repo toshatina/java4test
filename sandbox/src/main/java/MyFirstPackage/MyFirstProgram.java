4ca362341abddc844aed4427b2b95118b18fff60
toshatina <toshatina@gmail.com>
2021-05-13 15:31:23 +0300

    засовываем метод в класс и не работает

--- sandbox/src/main/java/MyFirstPackage/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstPackage/MyFirstProgram.java
@@ -6,10 +6,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
     Point A = new Point(1,1);
     Point B = new Point(5,4);
-        System.out.println("расстояние между точками А и Б = " + distance(A,B));
+        System.out.println("расстояние между точками А и Б = " + Point.distance(A,B));
     }
-        public static double distance(Point X1,Point X2){
-            return (sqrt((X1.x-X2.x)*(X1.x-X2.x)+(X1.y-X2.y)*(X1.y-X2.y)));
-        }
         }
 
--- sandbox/src/main/java/MyFirstPackage/Point.java
+++ sandbox/src/main/java/MyFirstPackage/Point.java
@@ -1,11 +1,20 @@
 package MyFirstPackage;
 
+import static java.lang.Math.sqrt;
+
 public class Point {
     double x;
     double y;
-    public Point(double x, double y)
-    {
+
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+    Point A = new Point(1,1);
+    Point B = new Point(1,1);
+    public static double distance(Point A, Point B) {
+        return (sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y)));
+    }
+
 }
+
